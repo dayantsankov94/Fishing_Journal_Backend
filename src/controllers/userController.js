41b45a55924884bc3f3e050e05f8d79567c11688
dayantsankov94 <daqntsankov@gmail.com>
2022-11-01 12:57:15 +0200

    upgrade

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const userService = require('../services/userService');
+const { isAuth } = require('../middlewares/guards');
 
 router.post('/add',
     isAuth,
